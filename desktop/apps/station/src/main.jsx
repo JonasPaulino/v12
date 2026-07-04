dc89a7a7a8e1725e9e124d5fef2d6a2273f1d915
Jonas Paulino <jonaspaulinodasilva@gmail.com>
2026-07-04 18:51:06 -0300

    correção de bug

--- desktop/apps/station/src/main.jsx
+++ desktop/apps/station/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
 import "./styles/global.css";
-import "sweetalert2/dist/sweetalert2.min.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
